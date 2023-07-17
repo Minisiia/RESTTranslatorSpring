beb8d108ec286f51f9da3aa48c3ff9eb8bed1283
Minisiia <ar_tik@yahoo.com>
2023-07-17 21:37:43 +0300

    6. Web service. rest & soap

--- src/main/java/com/example/RESTTranslatorSpring/controller/TranslateDBController.java
+++ src/main/java/com/example/RESTTranslatorSpring/controller/TranslateDBController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TranslateDBController {
-
     public TranslateDBService  translateDBService;
     @Autowired
     public TranslateDBController(TranslateDBService translateDBService) {
